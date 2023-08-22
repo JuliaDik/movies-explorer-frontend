635335bffd80a300bd23d8e487db12b7925dd9b7
Julia Dikolenko <tsvetkova-julia-vl@yandex.ru>
2023-08-23 02:11:48 +0300

    feat: добавить прелоадер

--- src/components/Preloader/Preloader.jsx
+++ src/components/Preloader/Preloader.jsx
@@ -1,1 +1,14 @@
-// отвечает за работу прелоадера
+// ПРЕЛОАДЕР
+import "./Preloader.css";
+
+function Preloader() {
+  return (
+    <div className="preloader">
+      <div className="preloader__container">
+        <span className="preloader__round"></span>
+      </div>
+    </div>
+  );
+}
+
+export default Preloader;
